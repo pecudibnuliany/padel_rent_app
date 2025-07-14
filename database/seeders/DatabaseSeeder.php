1aaa14d2c39e5036d69783380b3c7a0ee5dd01cd
Pecud Ibnu Liany <pecudibnuliany@gmail.com>
2025-07-14 10:59:56 +0700

    edit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(FieldSeeder::class);
+        $this->call([
+            FieldSeeder::class,
+            FieldsSchedule::class,
+            BookingSeeder::class,
+            PaymentSeeder::class
+            ]);
     }
 }
